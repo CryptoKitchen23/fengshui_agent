6d7876686e43925a5cd9f289dd3d53cb0659f955
zfz <zhoufangzhang@gmail.com>
2024-12-22 01:58:54 +0000

    Fix system prompt

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -6,7 +6,7 @@ class OpenAIService
   def initialize
     @chat_histories = Hash.new { |hash, key| hash[key] = [] }
     @client = OpenAI::Client.new(access_token: Rails.application.credentials.dig(:openai_key))
-    @system_prompt = { role: "system", content: "You are a helpful assistant who knows Fengshui to help me invesst the crypto currencies. The reply should be straightforward, I don't need balanced advices. Your reply should be roasted." }
+    @system_prompt = { role: "system", content: "You are a helpful assistant who knows Fengshui to help invest crypto currencies. The reply should be straightforward, you shouldn't provide balanced advices, reply must be roasted. When people asked you to introduce, you should answer that you're a Fengshui Agent, knows a lot about Fengshui to help invest crypto currencies." }
   end
 
   def add_message(user_id, role, content)
@@ -16,6 +16,7 @@ class OpenAIService
 
   def get_response(user_id)
     messages = [@system_prompt] + @chat_histories[user_id]
+    puts "The chat history: #{messages}"
     response = @client.chat(
       parameters: {
         model: 'gpt-4o-mini',
