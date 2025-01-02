b0bfef808c412706bd5a244b2f48b13f48cada6f
zfz <zhoufangzhang@gmail.com>
2025-01-02 02:19:40 +0000

    Fix: system prompt in openai service

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -3,16 +3,16 @@ require_relative "cmc_pump_fun_service"
 
 class OpenAIService
   MAX_HISTORY_LENGTH = 15
+  COIN_FETCH_INTERVAL = 24 * 60 * 60 # 1 day in seconds
 
   def initialize
     @chat_histories = Hash.new { |hash, key| hash[key] = [] }
     @client = OpenAI::Client.new(access_token: Rails.application.credentials.dig(:openai_key))
-    load_prompts
+    @last_coin_fetch_time = Time.now - COIN_FETCH_INTERVAL
+    @coins = []
 
-    coins = get_pump_fun_recommendations()
-    if coins.any?
-      @system_prompt[:content] += "\n #{coins} \n #{@pump_fun_prompt[:content]} "
-    end
+    load_prompts
+    fetch_coins_if_needed
   end
 
   def load_prompts
@@ -23,6 +23,16 @@ class OpenAIService
 
   def reload_prompts
     load_prompts
+    fetch_coins_if_needed
+    @system_prompt[:content] += "\n #{@coins.join(', ')} \n #{@pump_fun_prompt[:content]} " if @coins.any?
+  end
+
+  def fetch_coins_if_needed
+    if Time.now - @last_coin_fetch_time >= COIN_FETCH_INTERVAL
+      @coins = get_pump_fun_recommendations
+      puts "Fetched coins: #{@coins}"
+      @last_coin_fetch_time = Time.now
+    end
   end
 
   def add_message(user_id, role, content)
